948e45e72cfdb44cb6aeb3295c52c6cb60ba280a
HDS <jonathan.canales.hds@gmail.com>
2021-04-15 17:23:33 -0500

    Sexta clase de JavaScript

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-// MAP AND FILTER
-let array = [1,2,3,4,5,6,7,8,9]
 
-    // Map: retorna un array con los elememtos tratados segun el operador
-    let newArray = array.map( item => item * item )
-    console.log(newArray);
+//COMPARACION DE ASYNC Y UN PROMISE
 
-    // Filter: retorna un arreglo con los elementos filtrados segun la condición
-    let newArray2 = array.filter( item => item > 5 )
-    console.log(newArray2);
-    
-    //Find: retorna el primer elemento que cumple la condición
-    let newArray3 = array.find( item => item%3 == 0)
-    console.log(newArray3);
+/*  //async
+let getNombre = async() => {
+    return 'Jonathan Canales Chavez'
+}*/
+
+/*  //promise
+let getNombre = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            resolve('Jonathan Canales')
+        },3000 )    //tiempo de espera en respuesta con el setTimeout
+    })
+}
+
+/* //Llamada de la funcion getNombre
+getNombre().then( nombre => {
+    console.log(nombre)
+}).catch( e => {
+    console.log('error de async')
+})*/
 
 
-    // NOTA: estos métodos retornan un nuevo array y no alteran el existente
-    for( i = 0; i < array.length; i++) {
-        console.log(array[i]);
-    }
+//APLICANDO ASYNC - AWAIT
 
-    let listObject = [
-        { id: 1, name: 'Jonathan' },
-        { id: 2, name: 'Carlos' },
-        { id: 3, name: 'Jilio' },
-        { id: 4, name: 'Steve' },
-    ]
-    for (const iterator of listObject) {
-        console.log(iterator);
-    }
+let getNombre = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            resolve('Jonathan Canales')
+        },3000 )    //tiempo de espera en respuesta con el setTimeout
+    })
+}
 
-    listObject.forEach(element => {
-        console.log(element.name);
-    });
+
+let mostrarNombre = async () => {
+    let name = await getNombre()
+    return `Su nombre es: ${name}`
+}
+
+mostrarNombre().then( mensaje => {
+    console.log(mensaje)
+} )
